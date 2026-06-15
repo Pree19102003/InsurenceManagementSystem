2209b3b07578116caee893e86c7b9d9f77dbb087
Preethi G N <preethign190@gmail.com>
2026-06-15 13:56:43 +0530

    static updated in setting.py

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -78,12 +78,7 @@ WSGI_APPLICATION = 'insurancemanagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
@@ -123,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS=[
 STATIC_DIR,
