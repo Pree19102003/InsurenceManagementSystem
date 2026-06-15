81bb6ecd85f5660a56fda135531169ff92f0b0d8
Preethi G N <preethign190@gmail.com>
2026-06-15 14:43:47 +0530

    db in setting updated

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -78,7 +78,15 @@ WSGI_APPLICATION = 'insurancemanagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
